69bad976fb8735b5a38fdeebac27ef77a17fdd4a
Thecodingpanda <th3codingpanda@gmail.com>
2025-09-17 01:35:57 +0200

    Added place walls (wip)
    Added wall place but they are half in the ground and u can still place on the same spot

--- Unity_Boips_TD/Assets/Scripts/GridHandler.cs
+++ Unity_Boips_TD/Assets/Scripts/GridHandler.cs
@@ -3,46 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridHandler : MonoSingleton<GridHandler>
-{
+{   
+    private InputPackageManagerScript _inputScript; 
     [SerializeField] private Camera rayCastCamera;
     [SerializeField] private LayerMask placementlayermask;
     [SerializeField] private GameObject gridPositionIndicator;
     [SerializeField] private Grid grid;
     [SerializeField] private Vector3 gridIndicatorOffset;
+    [SerializeField] private GameObject gridDisplay;
+    [SerializeField] private List<GameObject> towers;
     private Vector3 _lastPosition;
-    private Dictionary<Vector3Int, GameObject> placedTowers = new Dictionary<Vector3Int, GameObject>();
+    private Dictionary<Vector3Int, GameObject> _placedWalls = new Dictionary<Vector3Int, GameObject>();
     Ray _ray;
     RaycastHit _hit;
-    private void GetSelectedMapPosition()
+    
+    private void Start()
     {
-
-        _ray = new Ray(rayCastCamera.transform.position, rayCastCamera.transform.forward);
-        if (Physics.Raycast(_ray, out _hit, 3, placementlayermask))
-        {
-            _lastPosition = _hit.point;
-        }
-        Vector3 cameraRayCastPosition = _lastPosition;
-        Vector3Int gridposition = grid.WorldToCell(cameraRayCastPosition);
-        gridPositionIndicator.transform.position = grid.CellToWorld(gridposition) + gridIndicatorOffset;
-
+        _inputScript = InputPackageManagerScript.Instance;
+        _inputScript.BuildMenuEvent.AddListener(ToggleBuildMode);
+        _inputScript.PlaceTowerEvent.AddListener(PlaceTower);
     }
-
-    private void GetTowerPlacement()
-    {
-        
-        _ray = new Ray(rayCastCamera.transform.position, rayCastCamera.transform.forward);
-        if (Physics.Raycast(_ray, out _hit, 3, placementlayermask))
-        {
-            _lastPosition = _hit.point;
-        }
-        Vector3 cameraRayCastPosition = _lastPosition;
-        Vector3Int gridposition = grid.WorldToCell(cameraRayCastPosition);
-        gridPositionIndicator.transform.position = grid.CellToWorld(gridposition) + gridIndicatorOffset;
-        Instantiate(gridPositionIndicator, gridPositionIndicator.transform.position, Quaternion.identity);
-    }
-
     private void Update()
     {
         GetSelectedMapPosition();
+    }
+
+
+    private void ToggleBuildMode()
+    {
+        gridDisplay.SetActive(!gridDisplay.activeSelf);
+        gridPositionIndicator.SetActive(!gridPositionIndicator.activeSelf);
+        
+    }
+    private void PlaceTower()
+    {
+        if (gridDisplay.activeSelf)
+        {
+            Instantiate(towers[0], gridPositionIndicator.transform.position , towers[0].transform.rotation);
+        }
+    }
+    
+    
+    private void GetSelectedMapPosition()
+    {
+        _ray = new Ray(rayCastCamera.transform.position, rayCastCamera.transform.forward);
+        if (Physics.Raycast(_ray, out _hit, 3, placementlayermask))
+        {
+            _lastPosition = _hit.point;
+        }
+        Vector3 cameraRayCastPosition = _lastPosition;
+        Vector3Int gridposition = grid.WorldToCell(cameraRayCastPosition);
+        gridPositionIndicator.transform.position = grid.CellToWorld(gridposition) + gridIndicatorOffset;
+
     }
 }
--- Unity_Boips_TD/Assets/Scripts/PlayerActions.cs
+++ Unity_Boips_TD/Assets/Scripts/PlayerActions.cs
@@ -3,36 +3,14 @@ using UnityEngine;
 
 public class PlayerActions : MonoBehaviour
 {
-    private InputPackageManagerScript _inputScript; 
-    [SerializeField]private GameObject gridDisplay,gridIndicator;
-    GridHandler _gridHandler;
     
-    void Start()
-    {
-        _gridHandler = GridHandler.Instance;
-        _inputScript = InputPackageManagerScript.Instance;
-        _inputScript.BuildMenuEvent.AddListener(ToggleBuildMode);
-        _inputScript.PlaceTowerEvent.AddListener(PlaceTower);
-    }
+    
 
-    void Update()
-    {
-        if (gridDisplay.activeSelf)
-        {
 
-        }
-    }
 
-    private void ToggleBuildMode()
-    {
-        gridDisplay.SetActive(!gridDisplay.activeSelf);
-        gridIndicator.SetActive(!gridIndicator.activeSelf);
-        
-    }
 
-    private void PlaceTower()
-    {
-        
-    }
+    
+
+
 
 }
