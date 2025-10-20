5c12d0d9324192d01cec603d409fc47d05be0a22
Th3codingpanda <RafWoud@pm.me>
2025-10-20 12:46:58 +0200

    Update PlayerAttack.cs

--- Unity_Boips_TD/Assets/Scripts/PlayerFolder/PlayerAttack.cs
+++ Unity_Boips_TD/Assets/Scripts/PlayerFolder/PlayerAttack.cs
@@ -2,78 +2,61 @@ using EnemyFolder;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-namespace Player
-{
-    public class PlayerAttack : MonoBehaviour
-    {
-        private InputManager input;
-        void Start()
-        {
-            input = InputManager.Instance;
-            input.AttackEvent.AddListener(Attack);
-=======
 namespace PlayerFolder
 {
-    public class PlayerAttack : MonoBehaviour
+
+
+    namespace PlayerFolder
     {
-        private InputManager _inputScript;
-        void Start()
+        public class PlayerAttack : MonoBehaviour
         {
-            _inputScript = InputManager.Instance;
-            _inputScript.AttackEvent.AddListener(Attack);
->>>>>>> Stashed changes
-=======
-namespace PlayerFolder
-{
-    public class PlayerAttack : MonoBehaviour
-    {
-        private InputManager _inputScript;
-        void Start()
-        {
-            _inputScript = InputManager.Instance;
-            _inputScript.AttackEvent.AddListener(Attack);
->>>>>>> Stashed changes
-        }
+            private InputManager _inputScript;
 
-        // Update is called once per frame
-
-        void Update()
-        {
-       
-        }
-
-        private void Attack()
-        {
-            Debug.Log("Attack");
-            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+            void Start()
             {
-                DamageScript enemy = hit.transform.GetComponent<DamageScript>();
-                if (enemy != null)
-                {
-                    enemy.TakeDamage(25);
+                _inputScript = InputManager.Instance;
+                _inputScript.AttackEvent.AddListener(Attack);
 
+            }
+
+            // Update is called once per frame
+
+            void Update()
+            {
+
+            }
+
+            private void Attack()
+            {
+                Debug.Log("Attack");
+                Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    DamageScript enemy = hit.transform.GetComponent<DamageScript>();
+                    if (enemy != null)
+                    {
+                        enemy.TakeDamage(25);
+
+                    }
                 }
             }
+
+            //if (Mouse.current.leftButton.wasPressedThisFrame)
+            //{
+            //    Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            //    RaycastHit hit;
+
+            //    if (Physics.Raycast(ray, out hit))
+            //    {
+            //        DamageScript enemy = hit.transform.GetComponent<DamageScript>();
+            //        if (enemy != null)
+            //        {
+            //            enemy.TakeDamage(10);
+            //        }
+            //    }
+            //}
         }
-
-        //if (Mouse.current.leftButton.wasPressedThisFrame)
-        //{
-        //    Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-        //    RaycastHit hit;
-
-        //    if (Physics.Raycast(ray, out hit))
-        //    {
-        //        DamageScript enemy = hit.transform.GetComponent<DamageScript>();
-        //        if (enemy != null)
-        //        {
-        //            enemy.TakeDamage(10);
-        //        }
-        //    }
-        //}
     }
 }
