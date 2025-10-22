0c8aa8352aa7839490fe294e0239deb257bdfd7d
Jessikxa <iamjessicadurkin@gmail.com>
2025-10-22 13:28:31 +0200

    menu fix

--- Unity_Boips_TD/Assets/Scripts/UIFolder/PauseMenu.cs
+++ Unity_Boips_TD/Assets/Scripts/UIFolder/PauseMenu.cs
@@ -2,27 +2,55 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    private InputManager input;
+    private bool _isPaused = false;
+    public GameObject PauseMenuUI;
+    private InputManager _input;
     private void Start()
     {
-        input = InputManager.Instance;
-        input.PauzeGame.AddListener(PauseGame);
+        //Time.timeScale = 0f;
+        _input = InputManager.Instance;
+        _input.PauzeGame.AddListener(TogglePause);
+    }
+
+    private void TogglePause()
+    {
+        if (_isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
     }
     public void PauseGame()
     {
         Time.timeScale = 0f;
-        gameObject.SetActive(true);
+        PauseMenuUI.SetActive(true);
+        _isPaused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1f;
-        gameObject.SetActive(false);
+        PauseMenuUI.SetActive(false);
+        _isPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void MouseSensitivity()
+    {
+
     }
 }
     
